8bf5dcdeeeefc05a5678d1a08557ca7b6233ecfd
Jim Hillhouse <jdhouse4@me.com>
2020-10-17 17:39:20 -0500

    Initial Commit

---
+++ SwiftUISceneKitDemo/SwiftUISceneKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISceneKitDemoApp.swift
+//  SwiftUISceneKitDemo
+//
+//  Created by James Hillhouse IV on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISceneKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
